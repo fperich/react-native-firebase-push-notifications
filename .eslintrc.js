6a18097f8e63d8439008f0aebfb0c893c6195c82
Bockdan David Porras <bockdandavid.porras@externos-cl.cencosud.com>
2019-04-17 15:34:37 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "no-console": 0, // Deshabilita error al hacer console.log
+        "indent": [2, 2], // Identación de 2 espacios.
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }] // Permite que archivos de React.JS sean .JSX y .JS
+    }
+};
